d744259eee2376aeb866da739bb0e97fab656e4a
Mohammad Al-Shalabi <91227088+Malshalab@users.noreply.github.com>
2022-08-16 13:49:32 -0400

    Time: 41 ms (5.42%), Space: 13.2 MB (84.03%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        sums=num1+num2
+        return sums
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        
